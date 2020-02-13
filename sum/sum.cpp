82f3365c7767fedcb9e4fff933858ea85b95cf64
Mucahit Kurtlar <mucahit_fb07@hotmail.com>
2020-02-12 21:08:23 -0500

    Initial commit

---
+++ sum/sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num1, num2, sum;
+	cout << "Please enter an integer:\n";
+	cin >> num1;
+	cout << "Please enter an integer again:\n";
+	cin >> num2;
+	sum = num1 + num2;
+	cout << "The sum of the numbers is " << sum << endl;
+	return 0;
+}
